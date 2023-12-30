8499184619c5c455c9484ade6ce9dc84fda66ec0
bunju20 <ghkd2271@naver.com>
2023-12-30 13:22:42 +0900

    feat: 연습3 끝냄

---
+++ 3_RC/lib/screen/home_screen.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: Container(
+          color: Colors.black,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                color: Colors.black,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      color: Colors.red,
+                      width: 50.0,
+                      height: 50.0,
+                    ),
+                    Container(
+                      color: Colors.orange,
+                      width: 50.0,
+                      height: 50.0,
+                    ),
+                    Container(
+                      color: Colors.yellow,
+                      width: 50.0,
+                      height: 50.0,
+                    ),
+                    Container(
+                      color: Colors.green,
+                      width: 50.0,
+                      height: 50.0,
+                    ),
+                  ],
+                ),
+
+              ),
+              Container(
+                color: Colors.black,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      color: Colors.orange,
+                      width: 50.0,
+                      height: 50.0,
+                    ),
+                  ],
+                ),
+
+              ),
+              Container(
+                color: Colors.black,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      color: Colors.red,
+                      width: 50.0,
+                      height: 50.0,
+                    ),
+                    Container(
+                      color: Colors.orange,
+                      width: 50.0,
+                      height: 50.0,
+                    ),
+                    Container(
+                      color: Colors.yellow,
+                      width: 50.0,
+                      height: 50.0,
+                    ),
+                    Container(
+                      color: Colors.green,
+                      width: 50.0,
+                      height: 50.0,
+                    ),
+                  ],
+                ),
+
+              ),
+              Container(
+                color: Colors.black,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      color: Colors.green,
+                      width: 50.0,
+                      height: 50.0,
+                    ),
+                  ],
+                ),
+
+              ),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
