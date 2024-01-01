86981c6d9d6a7e4a82603da6704c26d95a5e54d3
bunju20 <ghkd2271@naver.com>
2024-01-01 23:07:13 +0900

    fix: 사진이 부드럽게 움직이도록 수정

--- 4_image/lib/home_screen.dart
+++ 4_image/lib/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,10 +10,41 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Timer? timer;
+  PageController controller = PageController(
+    initialPage: 0,
+  );
+
+  @override
+  void initState() {
+    timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      int currentPage = controller.page!.toInt();
+      int nextPage = currentPage + 1;
+      if (nextPage > 4) nextPage = 0;
+      controller.animateToPage(nextPage,
+          duration: Duration(milliseconds: 400),
+          curve: Curves.linear,
+      );
+    });
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    if (timer != null) {
+      timer!.cancel();
+    }
+    // TODO: implement dispose
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
+        controller: controller,
         children: [1, 2, 3, 4, 5]
             .map((e) => Image.asset(
                   'asset/img/image_$e.jpeg',
